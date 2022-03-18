1e79f845266a9ae0ce772f7c5276e5a581492134
aberraxao <diana_milu@live.com.pt>
2022-03-18 00:37:35 +0000

    Step 1 done

--- parte-1/lista_condutores.sh
+++ parte-1/lista_condutores.sh
@@ -3,10 +3,20 @@
 ###############################################################################
 ## ISCTE-IUL: Trabalho prático de Sistemas Operativos 2021/2022
 ##
-## Aluno: Nº:       Nome: 
+## Aluno: Nº: 103678 Nome: Diana Andreia de Oliveira Amaro
 ## Nome do Módulo: lista_condutores.sh
 ## Descrição/Explicação do Módulo: 
-##
+## TODO
 ##
 ###############################################################################
 
+fi="pessoas.txt"
+fo="condutores.txt"
+if [ -f ${fi} ]; then
+  # pessoas.txt format: <ID carta condução>:<Nome>:<Nr Contribuinte>:<Contacto>
+  # condutores.txt format: <ID>-<Nome>;<ID carta condução>;<Contacto>;<Nr Contribuinte>;<Saldo (em créditos)>
+  awk -F[:] '{print "ID" $3 "-" $2 ";" $1 ";" $4 ";" $3 ";150"}' ${fi} > ${fo}
+  ./success 2 ${fo}
+else
+  ./error 1 ${fi}
+fi

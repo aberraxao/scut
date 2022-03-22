a95d3b4f4067fc644444c3f147cbe074211f4447
aberraxao <diana_milu@live.com.pt>
2022-03-22 01:17:47 +0000

    Revamp

--- parte-1/menu.sh
+++ parte-1/menu.sh
@@ -10,7 +10,8 @@
 ##
 ###############################################################################
 
-until [ '${m}' = '0' ]; do
+# Apresenta o menu até ser selecionada a opção '0'
+until [ "${m}" = "0" ]; do
 
   # Lista as opções disponíveis
   echo '1. Listar condutores'
@@ -20,32 +21,33 @@ until [ '${m}' = '0' ]; do
   echo '0. Sair'
   
   # Pede ao utilizador para escolher uma opção
-  echo ''
-  read -p 'Opção: ' m
-  echo ''
+  read -p $'\nOpção: ' m
 
   case ${m} in
   
     1)
-      echo 'Listar condutores'
-      echo ''
+      # 1. Listar condutores
+      echo $'\nListar condutores\n'
       ./lista_condutores.sh
       read ;;
       
     2)
-      echo 'Altera taca de portagem'
-      echo ''
+      # 2. Altera taxa de portagem
+      echo $'\nAltera taxa de portagem\n'
+      
+      # Pede ao utilizador para preencher os inputs
       read -p 'Lanço           : ' l
       read -p 'Auto-estrada    : ' ae
       read -p 'Novo valor taxa : ' tx
+      
       ./altera_taxa_portagem.sh ${l} ${ae} ${tx}
       read ;;
       
     3)
-      echo 'Stats'
-      echo ''
-      
-      until [ '${sm}' = '0' ]; do
+      # 3. Stats
+      echo $'\nStats\n'
+   
+      until [ "${sm}" = "0" ]; do
 
         # Lista as opções disponíveis
         echo '1. Nome de todas as Autoestradas'
@@ -54,53 +56,48 @@ until [ '${m}' = '0' ]; do
         echo '0. Voltar'
   
         # Pede ao utilizador para escolher uma opção
-        echo ''
-        read -p 'Opção: ' sm
+        read -p $'\nOpção: ' sm
 
         case ${sm} in
       
           1)
             # 1. Nome de todas as Autoestradas
-            ./stats.sh 'listar'
-            read ;;
+            ./stats.sh 'listar' ;;
        
           2)
             # 2. Registos de utilização
             read -p 'Mínimo de registos : ' r
-            echo $r
-            ./stats.sh 'registos' ${r} 
-            read ;;
+            ./stats.sh 'registos' ${r} ;;
         
           3)
             # 3. Listagem condutores
-            ./stats.sh 'condutores'
-            read ;;
+            ./stats.sh 'condutores' ;;
         
           0)
-            # Sai do menu
-            ;;
+            # 0. Voltar
+            break ;;
        
           *)
             # ERRO: O argumento recebido é inválido
-            ./error 3 ${sm}
-            read ;;
+            ./error 3 ${sm} ;;
         
         esac
-        break
         
-      done
+        read # Espera até uma tecla ser pressionada
+        break # Volta automaticamente ao menu anterior
+        
+      done ;;
       
-      ;;
     4)
-      echo 'Faturação'
-      echo ''
+      # 4. Faturação
+      echo $'\nFaturação\n'
+      
       ./faturacao.sh
       read ;;
-      
+     
     0)
-      # Sai do menu
-      break
-      ;;
+      # 0. Voltar
+      exit ;;
       
     *)
       # ERRO: O argumento recebido é inválido
